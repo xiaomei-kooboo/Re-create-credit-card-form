0bd6e703bea41ef97d0c6560928a0d516df5de98
xiaomei-kooboo <xiaomei@kooboo.net@example.com>
2018-07-12 17:54:46 +0800

    Re-create-creditcard

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Creat from '@/components/creat.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: resolve => { require(['../components/creat.vue'], resolve) }
+    }
+  ]
+})
+  
